947a8a837201a3b4e2ab67a77d3089dc04daef9c
Gani25 <memonabdulgani3d@gmail.com>
2025-08-18 11:31:35 +0530

    Update day3.sql

--- day3.sql
+++ day3.sql
@@ -42,5 +42,26 @@ select * from actor
 where first_name like "A__E%";
 
 -- find actor with first name contains R at any place
--- and last_name ends with S
-select * from actor;
+-- and last_name ends with S -> 2conditions -true -> and
+select * from actor
+where first_name like "%R%" and last_name like "%S";
+
+-- limit 
+select * from actor
+where actor_id <=5;
+-- top 5 rows
+select * from actor
+limit 5;
+select * from actor
+limit 10;
+
+-- limit / offset 
+-- find actors between 10 to 20
+select * from actor
+limit 10 offset 10;
+
+select * from actor
+limit 5 offset 3;
+
+select * from actor
+limit 1 offset 1;
