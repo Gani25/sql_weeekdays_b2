a1a97dbc0e6c0f47540a2b765ea57b43515156da
Gani25 <memonabdulgani3d@gmail.com>
2025-09-01 12:04:23 +0530

    Update day8.sql

--- day8.sql
+++ day8.sql
@@ -38,8 +38,35 @@ select * from student;
 
 truncate table student;
 select * from student;
--- Foreign Key
 
 insert into student(name,email,age) values
 ("Rohit Sharma","rhg12@gmail.com",25);
 select *  from student;
+
+-- foreign key	
+drop table student;
+drop table if exists student;
+
+create table course
+(
+	cid int primary key auto_increment,
+    cname varchar(50) not null,
+    cinstructor varchar(50) not null,
+    cprice int not null,
+    cduration varchar(100) not null
+);
+
+describe course;
+
+create table student
+(
+	roll_no int not null,
+    name varchar(50) not null,
+    phno varchar(15) not null,
+    cid int,
+    foreign key (cid) references course(cid)
+);
+
+describe student;
+
+show create table student;
